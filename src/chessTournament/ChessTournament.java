7c9f003fed407e79aaf9753b9f9ba9ba04dac486
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-10-01 22:37:55 +0530

    update

--- src/chessTournament/ChessTournament.java
+++ src/chessTournament/ChessTournament.java
@@ -7,31 +7,73 @@ public class ChessTournament {
     public static void main(String[] args) throws ExceptionHandler {
         ChessTournament chess = new ChessTournament();
         Scanner sc = new Scanner(System.in);
-        System.out.println("No of participants");
-        int playerCount = sc.nextInt();
-        System.out.println("No of rounds");
-        int rounds = sc.nextInt();
-        System.out.println("player details");
         List<Player> players = new ArrayList<>();
-        for (int i = 1; i <= playerCount; i++) {
-            Player player = new Player();
-            player.setPlayerName("player " + i);
-            player.setPlayerId(i);
-            players.add(player);
-        }
         TournamentDriver obj = new TournamentDriver();
-        obj.initialisePlayerList(players);
-        for (int i = 1; i <= rounds; i++) {
-            List<Map.Entry<Integer, Player>> list = obj.schedule();
-            chess.scheduleMatch(list);
-            System.out.println();
-            List<Map.Entry<Integer, Player>> list1 = obj.getResultOfRound(list);
-            chess.matchResult(list1);
-            List<Map.Entry<Integer, Player>> list2 = obj.getResultOfRound(list);
-            for (int k = 0; k < list2.size(); k++) {
-                String name = list2.get(k).getValue().getPlayerName();
-                System.out.println(name + " " + "(" + list2.get(k).getValue().getTotalPoint() + ")");
-            }
+        boolean end=true;
+        while (end)
+        {
+            System.out.println("1.Conduct chess tournament");
+            System.out.println("2.Display matches");
+            System.out.println("3.Rank list");
+            System.out.println("4.exit");
+            int choice=sc.nextInt();
+            switch (choice)
+            {
+                case 1: {
+                    System.out.println("No of participants");
+                    int playerCount = sc.nextInt();
+                    System.out.println("No of rounds");
+                    int rounds = sc.nextInt();
+
+
+                    for (int i = 1; i <= playerCount; i++) {
+                        Player player = new Player();
+                        player.setPlayerName("player " + i);
+                        player.setPlayerId(i);
+                        players.add(player);
+                    }
+                    obj.initialisePlayerList(players);
+
+
+                    System.out.println("player details");
+                    for (int i = 1; i <= rounds; i++) {
+                        System.out.println("Round:" + i);
+                        List<Map.Entry<Integer, Player>> list = obj.schedule();
+                        chess.scheduleMatch(list);
+
+                        System.out.println("Result for Round:" + i);
+                        list = obj.getResultOfRound(list);
+                        chess.matchResult(list);
+
+                        System.out.println("order of the player");
+                        list = obj.schedule();
+                        for (int k = 0; k < list.size(); k++) {
+                            String name = list.get(k).getValue().getPlayerName();
+                            System.out.println(name + " " + "(" + list.get(k).getValue().getTotalPoint() + ")");
+                        }
+                    }
+                }
+                break;
+                case 2:
+                {
+                    System.out.println("enter player id");
+                    int playerId= sc.nextInt();
+                    Player player= obj.getPlayer(playerId);
+                    System.out.println(player);
+
+                }
+                break;
+                case 3:
+                {
+
+                }
+                break;
+                case 4:
+                {
+
+                }
+         }
+
         }
     }
         private void scheduleMatch (List < Map.Entry < Integer, Player >> list)
